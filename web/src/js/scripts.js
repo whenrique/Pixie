b2a715c0fa2f69f999988f45e1e036bac0fcd888
whenrique <whp.zsf@gmail.com>
2014-09-25 01:28:47 -0300

    add folder src

---
+++ web/src/js/scripts.js
@@ -0,0 +1,20 @@
+(function($){
+
+	$('.menu a').on('click', function(e){
+		e.preventDefault();
+		var href = $(this).attr('href');
+
+		$('body > section').each(function(){
+			var id = $(this).attr('id');
+
+			if (href == id) {
+				$('html, body').animate({
+	                scrollTop: jQuery('#'+id).offset().top
+	            }, 800);
+
+	            console.log('lalala')
+			}
+		})
+	});
+
+})(jQuery);
